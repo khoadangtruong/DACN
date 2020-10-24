6d0c3e18a59b6cfa3c9f4b6d67e6f2faf79440a1
Khoa <khoadangtruong99@gmail.com>
2020-10-24 14:42:42 +0700

    User profile model

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,7 @@
+from user.models import UserProfile
 from django.contrib import admin
 
-# Register your models here.
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user_name', 'address', 'phone', 'city', 'country', 'image_tag']
+
+admin.site.register(UserProfile, UserProfileAdmin)
